1c80f55a85407de65abc1d87673ecbeffb5f0811
murilo-simoes <murilorsimoes@gmail.com>
2023-01-12 15:27:27 -0300

    fixxxxx

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
+module.exports = nextConfig;
